1f2ab99a4ca2468a04202561a85dcad3b0c38c9a
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2022-02-28 20:07:26 -0500

    Creando componente Navbar (Cliente)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './pages/clientes/navbar/navbar.component';
+import { HomeComponent } from './pages/clientes/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
