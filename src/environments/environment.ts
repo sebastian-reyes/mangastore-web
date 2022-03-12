57bf9980c330a1b0c546611ee41186e963c0c0a6
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2022-03-11 20:54:47 -0500

    Modificando environments para pruebas con localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const urlBase = "https://test-apimanga.herokuapp.com/api";
+const urlBase = "http://localhost:9898/api";
 
 export const environment = {
   production: false,
