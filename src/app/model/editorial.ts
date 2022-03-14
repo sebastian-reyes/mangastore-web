d93c84b123694531f7eb2377de2c44e94c464ae9
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2022-03-13 21:05:51 -0500

    Mejorando listado de mangas según demografía

---
+++ src/app/model/editorial.ts
@@ -0,0 +1,5 @@
+export class Editorial {
+    id_editorial: string;
+    nombre_editorial: string;
+    pais: string;
+}
--- src/app/model/manga.ts
+++ src/app/model/manga.ts
@@ -1,11 +1,14 @@
 import { Autor } from "./autor";
 import { Demografia } from "./demografia";
+import { Editorial } from "./editorial";
 
 export class Manga {
     id_manga: number;
     nombre_manga: string;
     sinopsis: string;
     foto: string;
+    numero_volumenes: number;
     autor: Autor;
     genero: Demografia;
+    editorial: Editorial;
 }
