f44932bed519194a602e576edc61f2dee4e0c560
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2023-08-09 19:04:53 -0500

    agregando validaciones de seguridad

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    if (!this.authService.estaAutenticado()) {
+      Swal.fire(
+        'No inició sesión',
+        'Usted no se encuentra autenticado.',
+        'warning'
+      );
+      this.router.navigate(['/home']);
+    }
+    if ((this.authService.obtenerDatosToken(localStorage.getItem('token')).authorities) != 'ROLE_ADMIN') {
+      Swal.fire(
+        'Recurso prohibido',
+        'Usted no cuenta con privilegios para ingresar a este recurso.',
+        'warning'
+      );
+      this.router.navigate(['/home']);
+    }
   }
 
 }
--- src/app/pages/clientes/carrito/carrito.component.ts
+++ src/app/pages/clientes/carrito/carrito.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/services/auth.service';
 import { CarritoService } from 'src/app/services/carrito.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carrito',
@@ -18,14 +20,24 @@ export class CarritoComponent implements OnInit {
 
   constructor(
     public carritoService: CarritoService,
-    public router: Router
+    public router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
+    if(!this.authService.estaAutenticado()){
+      Swal.fire(
+        'No inició sesión',
+        'Usted no se encuentra autenticado.',
+        'warning'
+      );
+      this.router.navigate(['/home']);
+    }
     this.carrito = this.carritoService.listarCarrito();
     for (let i of this.carrito) {
       this.subtotal = (i.producto.precio * i.cantidad) + this.subtotal;
     }
     this.total = this.subtotal - this.descuento;
+    
   }
 }
